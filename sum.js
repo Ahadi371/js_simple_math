0d3d583dc4fe6feeab323cfde8e55b44c4e2fb3b
mm <m.mazharulislam333@gmail.com>
2024-05-16 15:35:09 +0600

    math sum add

---
+++ sum.js
@@ -0,0 +1,8 @@
+const numbers =[44, 23, 34, 32, 54, 5, 78];
+let sum = 0;
+for (let i= 0; i<numbers.length; i++){
+const element = numbers[i];
+// console.log(element);
+  sum = sum + element;
+}
+console.log(sum);
