8742ecbe657324d7da6777bc347c1f970630aa66
mm <m.mazharulislam333@gmail.com>
2024-05-16 15:54:17 +0600

    function for of chilaici

--- sum.js
+++ sum.js
@@ -5,4 +5,15 @@ const element = numbers[i];
 // console.log(element);
   sum = sum + element;
 }
-console.log(sum);
+// console.log(sum);
+
+function arryTotal(numbers){
+    let sum = 0;
+    for(let i =0; i <numbers.length; i++){
+        const element = numbers[i];
+        sum = sum + element;
+    }
+    return sum;
+}
+const total = arryTotal(numbers)
+console.log(total);
