b3c6dd503ec992fc9321fccabf709169a27e4560
mm <m.mazharulislam333@gmail.com>
2024-05-20 15:21:34 +0600

    typof and if use

--- fibonacci.js
+++ fibonacci.js
@@ -4,6 +4,13 @@
 // }
 // console.log(fibo);
 function fibonaccSeries(num) {
+    if (typeof num != 'number'){
+        return 'please give a number'
+
+    }
+    if (num < 2){
+        return 'please enter a positive number greater than 2'
+    }
     const fibo = [4, 5];
     for (let i = 2; i<= num; i ++){
     fibo[i] = fibo[i -1] + fibo[i -2];
