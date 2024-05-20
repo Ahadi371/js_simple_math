dde6ba40af79468c03b2e87c06cdb9126cb36407
mm <m.mazharulislam333@gmail.com>
2024-05-20 13:19:25 +0600

    function appaly

--- fibonacci.js
+++ fibonacci.js
@@ -1,5 +1,15 @@
-const fibo = [5, 10];
-for (let i = 2; i<= 10; i ++){
-    fibo[i] =fibo[i - 1] + fibo[i - 2];
+// const fibo = [5, 10];
+// for (let i = 2; i<= 10; i ++){
+//     fibo[i] =fibo[i - 1] + fibo[i - 2];
+// }
+// console.log(fibo);
+function fibonaccSeries(num) {
+    const fibo = [4, 5];
+    for (let i = 2; i<= num; i ++){
+    fibo[i] = fibo[i -1] + fibo[i -2];
+
+    }
+    return fibo;
 }
-console.log(fibo);
+const fiboSeries = fibonaccSeries(13);
+console.log(fiboSeries);
